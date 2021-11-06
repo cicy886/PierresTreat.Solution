51294c9f5a3fc20626d8aa1943294795c4b67241
Sisi Vieira <cicy886@gmail.com>
2021-11-06 15:36:03 -0700

    new migration with addIdentity function

--- PierresTreat/Migrations/20211106223450_addIdentity.Designer.cs
+++ PierresTreat/Migrations/20211106223450_addIdentity.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PierresTreat.Models;
 
 namespace PierresTreat.Migrations
 {
     [DbContext(typeof(PierresTreatContext))]
-    partial class PierresTreatContextModelSnapshot : ModelSnapshot
+    [Migration("20211106223450_addIdentity")]
+    partial class addIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
