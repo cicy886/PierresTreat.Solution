dcb442d3ffeb19558467e2c373c5c00579ab3257
Sisi Vieira <cicy886@gmail.com>
2021-11-06 13:36:37 -0700

    add create route and corresponding view for treat

--- PierresTreat/Controllers/TreatsController.cs
+++ PierresTreat/Controllers/TreatsController.cs
@@ -6,19 +6,32 @@ using PierresTreat.Models;
 
 namespace PierresTreat.Controllers
 {
-  public class TreatsController: Controller
-  {
-    private readonly PierresTreatContext _db;
-
-    public TreatsController(PierresTreatContext db)
+    public class TreatsController : Controller
     {
-      _db = db;
-    }
+        private readonly PierresTreatContext _db;
 
-    public ActionResult Index()
-    {
-      List<Treat> model = _db.Treats.ToList();
-      return View(model);
+        public TreatsController(PierresTreatContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Treat> model = _db.Treats.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Treat treat)
+        {
+            _db.Treats.Add (treat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-  }
 }
