c4b0a69b7ffc0cef8d274510262229d7d51ccbeb
Sisi Vieira <cicy886@gmail.com>
2021-11-06 13:52:54 -0700

    add delete route and its corresponding view for treat

--- PierresTreat/Controllers/TreatsController.cs
+++ PierresTreat/Controllers/TreatsController.cs
@@ -46,17 +46,39 @@ namespace PierresTreat.Controllers
         }
 
         public ActionResult Edit(int id)
-    {
-      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
-      return View(thisTreat);
-    }
+        {
+            var thisTreat =
+                _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
 
-    [HttpPost]
-    public ActionResult Edit(Treat treat)
-    {
-      _db.Entry(treat).State = EntityState.Modified;
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+        [HttpPost]
+        public ActionResult Edit(Treat treat)
+        {
+            _db.Entry(treat).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var thisTreat =
+                _db
+                    .Treats
+                    .FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisTreat =
+                _db
+                    .Treats
+                    .FirstOrDefault(treat => treat.TreatId == id);
+            _db.Treats.Remove (thisTreat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
