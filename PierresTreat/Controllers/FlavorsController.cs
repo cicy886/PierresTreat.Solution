7eab1922a3b85f39ad19e309391a0e8276f1808d
Sisi Vieira <cicy886@gmail.com>
2021-11-06 14:05:35 -0700

    add create route and its corresponding view for flavor

--- PierresTreat/Controllers/FlavorsController.cs
+++ PierresTreat/Controllers/FlavorsController.cs
@@ -7,18 +7,40 @@ using PierresTreat.Models;
 
 namespace PierresTreat.Controllers
 {
-  public class FlavorsController : Controller
-  {
-    private readonly PierresTreatContext _db;
-
-    public FlavorsController(PierresTreatContext db)
+    public class FlavorsController : Controller
     {
-      _db = db;
-    }
+        private readonly PierresTreatContext _db;
 
-    public ActionResult Index()
-    {
-      return View(_db.Flavors.ToList());
+        public FlavorsController(PierresTreatContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_db.Flavors.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Flavor flavor, int TreatId)
+        {
+            _db.Flavors.Add (flavor);
+            _db.SaveChanges();
+            if (TreatId != 0)
+            {
+                _db
+                    .FlavorTreat
+                    .Add(new FlavorTreat()
+                    { TreatId = TreatId, FlavorId = flavor.FlavorId });
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-  }
 }
