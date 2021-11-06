1dd6ed906180da345d07c36c0e2120ccf5035516
Sisi Vieira <cicy886@gmail.com>
2021-11-06 15:33:37 -0700

    update file and configure he application to use identity with EF and MVC

--- PierresTreat/Startup.cs
+++ PierresTreat/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,43 +9,62 @@ using PierresTreat.Models;
 
 namespace PierresTreat
 {
-  public class Startup
-  {
-    public Startup(IWebHostEnvironment env)
+    public class Startup
     {
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddJsonFile("appsettings.json");
-      Configuration = builder.Build();
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder =
+                new ConfigurationBuilder()
+                    .SetBasePath(env.ContentRootPath)
+                    .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services
+                .AddEntityFrameworkMySql()
+                .AddDbContext<PierresTreatContext>(options =>
+                    options
+                        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"],
+                        ServerVersion
+                            .AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+
+            services
+                .AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<PierresTreatContext>()
+                .AddDefaultTokenProviders();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseAuthentication();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app
+                .UseEndpoints(routes =>
+                {
+                    routes
+                        .MapControllerRoute("default",
+                        "{controller=Home}/{action=Index}/{id?}");
+                });
+
+            app.UseStaticFiles();
+
+            app
+                .Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+        }
     }
-
-    public IConfigurationRoot Configuration { get; set; }
-
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddMvc();
-
-      services.AddEntityFrameworkMySql()
-        .AddDbContext<PierresTreatContext>(options => options
-        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
-    }
-
-    public void Configure(IApplicationBuilder app)
-    {
-      app.UseDeveloperExceptionPage();
-      app.UseRouting();
-
-      app.UseEndpoints(routes =>
-      {
-        routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-      });
-
-      app.UseStaticFiles();
-      
-      app.Run(async (context) =>
-      {
-        await context.Response.WriteAsync("Hello World!");
-      });
-    }
-  }
 }
