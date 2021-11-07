6c1c62a5590f37635c96549bd582b9f13f10e14a
Sisi Vieira <cicy886@gmail.com>
2021-11-06 17:21:19 -0700

    add styles to the app

--- PierresTreat/Migrations/20211106235555_Authorization2.Designer.cs
+++ PierresTreat/Migrations/20211106235555_Authorization2.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PierresTreat.Models;
 
 namespace PierresTreat.Migrations
 {
     [DbContext(typeof(PierresTreatContext))]
-    partial class PierresTreatContextModelSnapshot : ModelSnapshot
+    [Migration("20211106235555_Authorization2")]
+    partial class Authorization2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
